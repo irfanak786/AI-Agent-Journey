298e6946b033e4fb5a9c05f1cfa557d8dc0de0ae
irfanak786 <147413566+irfanak786@users.noreply.github.com>
2025-02-13 14:25:58 +0300

    Update trash_bin_cleaner.py
    Added code to the trash_bin_cleaner.py

--- trash_bin_cleaner.py
+++ trash_bin_cleaner.py
@@ -1,1 +1,13 @@
+environment = ["walls", "open area", "trash bin"]
+def decide_and_move(environment):
+    for element in environment:
+        print(f"checking: {element}")
+        if element == "walls":
+            print("A wall came in front of me and I am moving to another direction ")
+        elif element == "open area":
+            print("I am in the open area and can move freely")
+        elif element == "trash bin":
+            print("I found the trash bin and I'll take out all the trash from it")
+        print(".......")
+decide_and_move(environment)
 
